fcb5c3b0dcf63a3a5e17647261a0f388792c8b56
darius-wattimena <dariuswattimena@hotmail.com>
2019-09-04 10:36:20 +0200

    Removed the unneeded JsonHttpClient and moved the install of the gson serializer to the JsonHelper class

--- src/request/BaseTrelloRequest.kt
+++ src/request/BaseTrelloRequest.kt
@@ -1,10 +1,10 @@
 package com.example.request
 
-import com.example.JsonHttpClient
 import com.example.TrelloRequest
+import com.example.helper.JsonHelper
 import com.google.gson.Gson
 
 abstract class BaseTrelloRequest<T> : TrelloRequest<T> {
-    val client = JsonHttpClient().client
+    val client = JsonHelper.Client()
     val gson = Gson()
 }
